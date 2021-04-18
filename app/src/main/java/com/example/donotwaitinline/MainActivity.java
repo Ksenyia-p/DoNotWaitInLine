f4597d107674717a9054523129c69be460f76282
Ksenyia-p <kseniapetrashko16@gmail.com>
2021-04-18 23:40:30 +0300

    second commit

--- app/src/main/java/com/example/donotwaitinline/MainActivity.java
+++ app/src/main/java/com/example/donotwaitinline/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.donotwaitinline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.editTextTextPersonName);
+    }
+
+    public void Oder(View view) {
     }
 }
