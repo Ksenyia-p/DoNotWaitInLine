a16aa622d6dca335ef62777bd184f167d03a60a1
Ksenyia-p <kseniapetrashko16@gmail.com>
2021-04-19 20:12:50 +0300

    commit 11

--- app/src/main/java/com/example/donotwaitinline/Drinks.java
+++ app/src/main/java/com/example/donotwaitinline/Drinks.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,6 +22,11 @@ public class Drinks extends AppCompatActivity {
     TextView textViewDrinkAdd;
     String fullText;
     String name;
+    CheckBox checkBoxLemon;
+    CheckBox checkBoxMilk;
+    CheckBox checkBoxSugar;
+    StringBuilder additions=new StringBuilder();
+
 
 
     @Override
@@ -28,10 +35,13 @@ public class Drinks extends AppCompatActivity {
         setContentView(R.layout.activity_drinks);
         radioButtonC=findViewById(R.id.radioButtonCoffee);
         radioButtonT=findViewById(R.id.radioButtonTea);
-        spinnerC=findViewById(R.id.spinnerCoffe);
+        spinnerC=findViewById(R.id.spinnerCoffee);
         spinnerT=findViewById(R.id.spinnerTea);
         textViewDrinkAdd=findViewById(R.id.textViewDrinkAdd);
         textViewHello=findViewById(R.id.textViewWantDrink);
+        checkBoxLemon=findViewById(R.id.checkBoxLemon);
+        checkBoxMilk=findViewById(R.id.checkBoxMilk);
+        checkBoxSugar=findViewById(R.id.checkBoxSugar);
         Intent intent=getIntent();
         name=intent.getStringExtra("name");
         fullText=String.format("Hello %s. What kind of drink do you want?",name);
@@ -60,6 +70,18 @@ public class Drinks extends AppCompatActivity {
         }else {
             typeOfDrink=spinnerC.getSelectedItem().toString();
         }
+        if (checkBoxSugar.isChecked()){
+            additions.append("sugar").append("  ");
+        }
+        if (checkBoxMilk.isChecked()){
+            additions.append("milk").append("  ");
+        }
+        if (checkBoxLemon.isChecked()){
+            additions.append("lemon").append("  ");
+        }
+        String result=String.format("Client %s make the oder %s, with %s and type is %s",
+                name,drink,additions.toString(),typeOfDrink);
+        Log.d("result",result);
 
     }
 }
