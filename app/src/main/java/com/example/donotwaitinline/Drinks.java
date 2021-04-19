b2fcd5d31bbbfa58e5a085738d2ae4619624fb37
Ksenyia-p <kseniapetrashko16@gmail.com>
2021-04-19 16:43:49 +0300

    commit 7

--- app/src/main/java/com/example/donotwaitinline/Drinks.java
+++ app/src/main/java/com/example/donotwaitinline/Drinks.java
@@ -3,12 +3,33 @@ package com.example.donotwaitinline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class Drinks extends AppCompatActivity {
+    RadioButton radioButtonT;
+    RadioButton radioButtonC;
+    boolean aBooleanT=true;
+    Spinner spinnerT;
+    Spinner spinnerC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
+        radioButtonC=findViewById(R.id.radioButtonCoffee);
+        radioButtonT=findViewById(R.id.radioButtonTea);
+        spinnerC=findViewById(R.id.spinnerCoffe);
+        spinnerT=findViewById(R.id.spinnerTea);
+        boolean checkedT = (radioButtonT).isChecked();
+        boolean checkedC = (radioButtonC).isChecked();
+        if (checkedT==aBooleanT){
+            spinnerT.setVisibility(View.VISIBLE);
+            spinnerC.setVisibility(View.INVISIBLE);
+        }else if (checkedC==aBooleanT){
+            spinnerT.setVisibility(View.INVISIBLE);
+            spinnerC.setVisibility(View.VISIBLE);
+        }
     }
-}
+   }
